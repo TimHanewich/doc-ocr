ab6b91dbb5bfcd754c0efd9f4aec89861334eb8b
Tim Hanewich <tahanewich@live.com>
2025-05-15 13:43:13 -0400

    Optimized to new standard

--- src/Program.cs
+++ src/Program.cs
@@ -14,20 +14,23 @@ namespace DocOCR
     {
         public static void Main(string[] args)
         {
+            //Step 1 - read all!
+            //This reads a series of images in a folder using OCR and then puts the entire response as a custom JSON object
+            //ReadAll().Wait();
+
+            //Step 2 - conver the JSON to plain text
             FormatAll();
         }
 
         public static async Task ReadAll()
         {
-            string[] ImagePaths = System.IO.Directory.GetFiles(@"C:\Users\timh\Downloads\palmero hoa\images");
+            string[] ImagePaths = System.IO.Directory.GetFiles(@"C:\Users\timh\Downloads\smallpdf-convert-20250515-125854");
             
             //Collect
             List<ImageReadTask> ReadTasks = new List<ImageReadTask>();
             foreach (string path in ImagePaths)
             {
-                string p = path.Replace(".jpg", "");
-                int lastunder = p.LastIndexOf("_");
-                string num = p.Substring(lastunder + 1);
+                string num = Path.GetFileNameWithoutExtension(path);
                 int numi = Convert.ToInt32(num);
                 ImageReadTask irt = new ImageReadTask();
                 irt.Page = numi;
@@ -62,35 +65,22 @@ namespace DocOCR
 
             //Write
             Console.WriteLine("Writing...");
-            System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\palmero hoa\src\all.json", JsonConvert.SerializeObject(ReadTasksSorted));
+            System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\doc-ocr\OUTPUT.json", JsonConvert.SerializeObject(ReadTasksSorted));
             Console.WriteLine("Wrote!");
         }
 
-        public static void FormatAll()
-        {
-            string content = System.IO.File.ReadAllText(@"C:\Users\timh\Downloads\palmero hoa\src\all.json");
-            ImageReadTask[]? irts = JsonConvert.DeserializeObject<ImageReadTask[]>(content);
-            if (irts != null)
-            {
-                string all = "";
-                foreach (ImageReadTask irt in irts)
-                {
-                    all = all + irt.Read + Environment.NewLine + Environment.NewLine;
-                }
-                System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\palmero hoa\src\all.txt", all);
-            }
-        }
+
 
         public static async Task<string> ReadAsync(string path)
         {
             Stream s = System.IO.File.OpenRead(path);
             HttpRequestMessage req = new HttpRequestMessage();
             req.Method = HttpMethod.Post;
-            req.RequestUri = new Uri("https://testcomputervision20240514.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read");
+            req.RequestUri = new Uri("https://20250515-cv-ocr.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2024-02-01&features=read");
             req.Content = new StreamContent(s);
             req.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpeg");
-            req.Headers.Add("Ocp-Apim-Subscription-Key", "<KEY HERE!>");
-            
+            req.Headers.Add("Ocp-Apim-Subscription-Key", "<key here!>");
+
             //Request
             HttpClient hc = new HttpClient();
             HttpResponseMessage resp = await hc.SendAsync(req);
@@ -100,28 +90,51 @@ namespace DocOCR
                 throw new Exception("Response '" + resp.StatusCode.ToString() + " from API. Msg: " + content);
             }
 
-            //Extract
+            //Extract text
             JObject jo = JObject.Parse(content);
-            JToken? token = jo.SelectToken("readResult.content");
-            if (token != null)
+            string text = ResponseToOCR(jo);
+            return text;
+        }
+
+        //Convert the entire response from the API into plain text
+        public static string ResponseToOCR(JObject response)
+        {
+            JToken? linesTOKEN = response.SelectToken("readResult.blocks[0].lines");
+            if (linesTOKEN == null)
             {
-                string? ss = token.Value<string>();
-                if (ss != null)
+                throw new Exception("Unable to find lines property in API response");
+            }
+            JArray lines = (JArray)linesTOKEN;
+            string ToReturn = "";
+            foreach (JObject line in lines)
+            {
+                JProperty? prop_text = line.Property("text");
+                if (prop_text != null)
                 {
-                    return ss;
-                }
-                else
-                {
-                    throw new Exception("Unable to extract string from readResult content.");
+                    ToReturn = ToReturn + prop_text.Value.ToString() + Environment.NewLine;
                 }
             }
-            else
+            if (ToReturn.Length > 0)
             {
-                throw new Exception("Unable to find readResult in response!");
+                ToReturn = ToReturn.Substring(0, ToReturn.Length - 1); //cut off last new line
+            }
+            return ToReturn;
+        }
+
+        public static void FormatAll()
+        {
+            string content = System.IO.File.ReadAllText(@"C:\Users\timh\Downloads\doc-ocr\OUTPUT.json");
+            ImageReadTask[]? irts = JsonConvert.DeserializeObject<ImageReadTask[]>(content);
+            if (irts != null)
+            {
+                string all = "";
+                foreach (ImageReadTask irt in irts)
+                {
+                    all = all + irt.Read + Environment.NewLine + Environment.NewLine;
+                }
+                System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\doc-ocr\OUTPUT.txt", all);
             }
         }
-    
-
     
     }
 }
